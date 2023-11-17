a0bded61c54fe67584da84af00a9f6c10a5c370f
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-11-17 09:53:32 +0100

    Initial commit

---
+++ src/main/java/KlajdiNdoci/U5W3D5Project/U5W3D5ProjectApplication.java
@@ -0,0 +1,13 @@
+package KlajdiNdoci.U5W3D5Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W3D5ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W3D5ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/KlajdiNdoci/U5W3D5Project/U5W3D5ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package KlajdiNdoci.U5W3D5Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3D5ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
